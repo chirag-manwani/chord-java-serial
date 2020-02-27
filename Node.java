b8062e36238ee7f9e7e1e0a5872339e33fca856d
Chirag Manwani <cmkmanwani@gmail.com>
2020-02-28 02:22:25 +0530

    After a long battle with bugs, finally have working code. Node class can now add nodes correctly

--- Node.java
+++ Node.java
@@ -6,11 +6,11 @@ public class Node {
     public static int m;
 
     static {
-        m = 160;
+        m = 7;
         shift = new ArrayList<>();
         shift.add(BigInteger.ONE);
         BigInteger TWO = new BigInteger("2", 10);
-        for(int i=1; i<m; ++i) {
+        for(int i=1; i<=m; ++i) {
             shift.add(shift.get(i-1).multiply(TWO));
         }
     }
@@ -21,22 +21,25 @@ public class Node {
     private Node pred;
 
     Node(String nodeName) {
-        System.out.println(nodeName);
         String hash = Util.hash(nodeName);
 
-        this.nodeName = nodeName;        
+        this.nodeName = nodeName;
         this.nodeId = new BigInteger(hash, 16);
         this.fingerTable = new ArrayList<>();
 
         for(int i=0; i<m; ++i) {
             fingerTable.add(new Finger());
-            fingerTable.get(i).start = nodeId.add(shift.get(i));
+            fingerTable.get(i).start = nodeId.add(shift.get(i)).mod(shift.get(m));
         }
-        // System.out.println(hash);
     }
 
-    public BigInteger getNodeId() {
-        return nodeId;
+    Node(String nodeName, BigInteger nodeId) {
+        this(nodeName);
+        this.nodeId = nodeId;
+        for(int i=0; i<m; ++i) {
+            fingerTable.add(new Finger());
+            fingerTable.get(i).start = nodeId.add(shift.get(i)).mod(shift.get(m));
+        }
     }
 
     public Node findSuccessor() {
@@ -52,12 +55,12 @@ public class Node {
         Node pred = this;
         BigInteger predId = pred.nodeId;
         BigInteger succId = pred.findSuccessor().nodeId;
-        while(!Util.in(id, predId, succId)) {
-            pred = closestPrecedingFinger(id);
+
+        while(!Util.in(id, predId, succId, 1)) {
+            pred = pred.closestPrecedingFinger(id);
             predId = pred.nodeId;
             succId = pred.findSuccessor().nodeId;
         }
-
         return pred;
     }
 
@@ -65,7 +68,7 @@ public class Node {
         for(int i=m-1; i>=0; --i) {
             Finger f = fingerTable.get(i);
             BigInteger fId = f.node.getNodeId();
-            if(Util.in(fId, nodeId, id)) {
+            if(Util.in(fId, nodeId, id, 0)) {
                 return f.node;
             }
         }
@@ -81,21 +84,52 @@ public class Node {
         }
         else {
             initFingerTable(n);
-            updateOthers();
+            updateOthers(n);
             // moveKeys();
         }
     }
 
     private void initFingerTable(Node n) {
-        // fingerTable.get(0).node = n.findSuccessor()
+        fingerTable.get(0).node = n.findSuccessor(fingerTable.get(0).start);
+        pred = findSuccessor().pred;
+        findSuccessor().pred = this;
+        pred.fingerTable.get(0).node = this;
+
+        for(int i=1; i<m; ++i) {
+            BigInteger fStart = fingerTable.get(i).start;
+            BigInteger prevId = fingerTable.get(i-1).node.nodeId;
+            if(Util.in(fStart, nodeId, prevId, 2)) {
+                fingerTable.get(i).node = fingerTable.get(i-1).node;
+            }
+            else {
+                fingerTable.get(i).node = n.findSuccessor(fStart);
+            }
+        }
+        // System.out.println("--------------------Newly added node --------------");
+        // printFingerTable();
+        // System.out.println("---------------------------------------------------");
     }
 
-    private void updateOthers() {
-
+    private void updateOthers(Node n) {
+        for(int i=0; i<m; ++i) {
+            BigInteger id = nodeId.subtract(shift.get(i)).mod(shift.get(m));
+            Node p = n.findPredecessor(id);
+            if(p.nodeId.compareTo(nodeId) == 0) {
+                continue;
+            }
+            p.updateFingerTable(this, i);
+        }
     }
 
     private void updateFingerTable(Node s, int i) {
+        BigInteger fId = fingerTable.get(i).node.nodeId;
 
+        if(Util.in(s.nodeId, nodeId, fId, 2)) {
+            fingerTable.get(i).node = s;
+            if(pred.nodeId.compareTo(s.nodeId) != 0) {
+                pred.updateFingerTable(s, i);
+            }
+        }
     }
 
     private void stabilize() {
@@ -108,5 +142,29 @@ public class Node {
 
     private void fixFingers() {
 
+    }
+
+    /*
+    *   Utility Functions
+    */
+
+    public void printFingerTable() {
+        System.out.println("\nRouting Table for " + nodeId);
+        System.out.println("m\t\tstart\t\tNode");
+        for(int i=0; i<m; ++i) {
+            System.out.println(i + "\t\t" + fingerTable.get(i).start + "\t\t" + fingerTable.get(i).node.nodeId);
+        }
+    }
+
+    /*
+    * Getters and Setters
+    */
+
+    public String getName() {
+        return this.nodeName;
+    }
+
+    public BigInteger getNodeId() {
+        return nodeId;
     }
 }
